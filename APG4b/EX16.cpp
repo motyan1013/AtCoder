0ccbb14b48ca3099da2fd190b8bb2ea73cfe68fa
motyan1013 <motyan1013@gmail.com>
2024-05-16 17:05:20 +0900

    add_by_EX17

---
+++ APG4b/EX16.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  vector<int> data(5);
+  for (int i = 0; i < 5; i++) {
+    cin >> data.at(i);
+  }
+
+  // dataの中で隣り合う等しい要素が存在するなら"YES"を出力し、そうでなければ"NO"を出力する
+  // ここにプログラムを追記
+
+  int Flag = 0;
+  for (int i=0; i < 4; i++){
+    if(data.at(i) == data.at(i+1)){
+       Flag = true;
+       break;
+    } 
+  }
+
+  if(Flag){
+    cout << "YES" << endl;
+  }
+
+  else {
+    cout << "NO" << endl;
+  }
+}
